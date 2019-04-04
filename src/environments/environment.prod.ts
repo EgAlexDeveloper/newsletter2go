bd7eaecf07e529864b6a5090435bb075bdb5be92
Moustafa Ahmed <Moustafa.Ahmed@ibtikar.com>
2019-04-04 10:22:19 +0200

    add env details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: './assets/mocking/',
+  page: 1,
+  pageSize: 10
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: './assets/mocking/',
+  page: 1,
+  pageSize: 10
 };
 
 /*
